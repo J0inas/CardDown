05bd5df5ee0278696bd424d7efa465683eedfcde
Joinas <jgoldhammer@outlook.de>
2023-06-10 19:56:07 +0200

    Add structure of md_to_anki
    first draft of implementing "genanki" into the project

--- md_to_anki.py
+++ md_to_anki.py
@@ -1,6 +1,34 @@
-# import genanki
+import random
+import genanki
 from tags import *
 from learningcards import *
 
-# Add a way to generate anki-stacks from md-files
+# Generates anki-stacks from md-files
+def md_to_anki(
+    flashcards : list,  # ToDo: create as a new type LearningCardList 
+    deck_name : str,
+    ):
+    anki_deck = genanki.Deck(id_generator(),deck_name) # Optional ToDo: give a description for the deck
     
+    for i in range(len(flashcards)):
+        note = generate_anki_cards(flashcards[i])
+        anki_deck.add_note()
+    
+    card_package = genanki.Package(anki_deck)
+    # ToDo adding Mediafiles - genanki.media_files = ['sound.mp3', 'images/image.jpg']
+    card_package.write_to_file(deck_name + '.apkg')
+
+def generate_anki_cards(card : LearningCard):
+    """
+    Generates anki-notes from the given Learningcard.
+    """
+    model = genanki.BASIC_MODEL
+    pass
+
+def id_generator():
+    """
+    Creates a random id for the model- and deck-identification.
+    
+    returns: random number that can be used as an id
+    """
+    return random.randrange(1 << 30, 1 << 31)
