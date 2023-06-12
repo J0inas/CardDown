0e2ad657b9527f52c1f3fad28d12aea8f289acc6
Joinas <jgoldhammer@outlook.de>
2023-06-12 21:24:58 +0200

    Fix md_to_anki
    program is now able to return simple anki-flashcards from a md-file

--- src/md_to_anki.py
+++ src/md_to_anki.py
@@ -6,31 +6,38 @@ from md_to_html import card_content_to_html
 
 # Generates anki-stacks from md-files
 def md_to_anki(
-    flashcards : list,  # ToDo: create as a new type LearningCardList 
+    file_name : list, 
     deck_name : str,
     ):
     """
-    Creates an anki-deck from the given flashcard-list-
+    Creates an anki-deck from the given md-file.
     
     returns: message that anki-deck was created
     """
+    # currently only for one file
+    md_cards = load_one_file(file_name)
+    flashcards = parse_md_cards(md_cards)
+    
     anki_deck = genanki.Deck(id_generator(),deck_name)
     
-    for i in range(len(flashcards)):
-        note = generate_anki_cards(flashcards[i])
+    for card in flashcards:
+        note = anki_note(card)
         anki_deck.add_note(note)
     
     card_package = genanki.Package(anki_deck)
 
     card_package.write_to_file(deck_name + '.apkg')
+    print("Writing file was succesful!")
 
-def generate_anki_cards(card : LearningCard):
+def anki_note(card : LearningCard):
     """
     Generates anki-notes from the given Learningcard.
+    
+    returns: single anki-flashcard
     """
     model = genanki.BASIC_MODEL
-    front = card_content_to_html(card.get_front_content)
-    back = card_content_to_html(card.get_back_content)
+    front = card_content_to_html(card.get_front_content())
+    back = card_content_to_html(card.get_back_content())
     fields = [front, back]
 
     note = genanki.Note(model,fields)
@@ -46,5 +53,4 @@ def id_generator():
     return random.randrange(1 << 30, 1 << 31)
 
 # test
-md_cards = parse_md_cards("test_card.md")
-md_to_anki(md_cards, "test")
+md_to_anki("test_card.md", "test")
