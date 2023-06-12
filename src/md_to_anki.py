407df5747cc0dcd7fc8f986048566625d178ad33
Joinas <jgoldhammer@outlook.de>
2023-06-12 20:56:17 +0200

    Add generate_anki_cards
    function that generates anki notes from given learningcards that get transfered to html in the function

--- src/md_to_anki.py
+++ src/md_to_anki.py
@@ -8,14 +8,19 @@ def md_to_anki(
     flashcards : list,  # ToDo: create as a new type LearningCardList 
     deck_name : str,
     ):
-    anki_deck = genanki.Deck(id_generator(),deck_name) # Optional ToDo: give a description for the deck
+    """
+    Creates an anki-deck from the given flashcard-list-
+    
+    returns: message that anki-deck was created
+    """
+    anki_deck = genanki.Deck(id_generator(),deck_name)
     
     for i in range(len(flashcards)):
         note = generate_anki_cards(flashcards[i])
-        anki_deck.add_note()
+        anki_deck.add_note(note)
     
     card_package = genanki.Package(anki_deck)
-    # ToDo adding Mediafiles - genanki.media_files = ['sound.mp3', 'images/image.jpg']
+
     card_package.write_to_file(deck_name + '.apkg')
 
 def generate_anki_cards(card : LearningCard):
@@ -23,7 +28,15 @@ def generate_anki_cards(card : LearningCard):
     Generates anki-notes from the given Learningcard.
     """
     model = genanki.BASIC_MODEL
-    pass
+    
+    
+    
+    fields = [card.get_front_content, card.get_back_content]
+    
+    
+    note = genanki.Note(model,fields)
+    
+    return note
 
 def id_generator():
     """
@@ -32,3 +45,7 @@ def id_generator():
     returns: random number that can be used as an id
     """
     return random.randrange(1 << 30, 1 << 31)
+
+# test
+md_cards = parse_md_cards("test_card.md")
+md_to_anki(md_cards, "test")
