4b4c5a4449f77b195e23680a5942866e98336a35
Niggo <nicolaskautt01@gmail.com>
2023-07-31 23:43:57 +0200

    add theme for testing and try out the features

--- src/gui/user_interface.py
+++ src/gui/user_interface.py
@@ -1,7 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import askopenfilenames
+
+entry: ttk.Entry
+
+
+def get_filenames_and_set():
+
+    filenames = askopenfilenames()
+    index = 0
+    for name in filenames:
+        entry.insert(index=index, string=name)
+        index += 1
+
 
 padding = 10
+
 padding_x = padding
 padding_y = padding
 # Main window is called root by convention
@@ -13,17 +27,20 @@ root.geometry("300x150")
 frm = ttk.Frame(root, padding=100)
 frm.grid()
 
-ttk.Label(master=frm, text="Hello, World!").grid(column=0, row=0)
+ttk.Label(master=frm, text="Select Files for Parsing").grid(column=0, row=0)
 ttk.Button(master=frm, text="Quit", command=root.destroy).grid(column=0, row=8)
 
 input_frame = ttk.Frame(master=frm)
 input_frame.grid()
 entry = ttk.Entry(master=input_frame)
-button = ttk.Button(master=input_frame, text="Create Cards")
+button = ttk.Button(master=input_frame, text="Open Files",
+                    command=get_filenames_and_set)
+
 
 entry.grid(column=0, row=0)
 button.grid(column=1, row=0, padx=padding_x, pady=padding_y)
 input_frame.grid(column=0, row=1)
+
 
 # creating a widget can work like this
 # widget = WidgetName(container, **options)
